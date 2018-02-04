f5149fadc9fa745e3136868b72efae0dc34fb014
RobertAlexandruN <33713887+RobertAlexandruN@users.noreply.github.com>
2018-02-04 11:42:33 +0200

    Importing project

---
+++ Fight2Death/Battle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight2Death
+{
+    class Battle
+    {
+
+        public static void StartFight(Warrior warrior1, Warrior warrior2)
+        {
+            while(true)
+            {
+                if(GetAttackResult(warrior1, warrior2) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+                if (GetAttackResult(warrior2, warrior1) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+        }
+
+        public static string GetAttackResult(Warrior warriorA,Warrior warriorB)
+        {
+            double warAAttkAmt = warriorA.Attack();
+            double warBblkAmt = warriorB.Block();
+
+            double dmg2WarB = warAAttkAmt - warBblkAmt;
+
+            if (dmg2WarB > 0)
+            {
+                warriorB.Health = warriorB.Health - dmg2WarB;
+            }
+            else dmg2WarB = 0;
+
+            Console.WriteLine("{0} Attacks " +
+                "{1} and Deals {2} Damage"+"\nAttack value of {0} was {3}" +
+                "\nBlock value of {1} was {4}",
+                warriorA.Name, 
+                warriorB.Name,
+                dmg2WarB,
+                warAAttkAmt,
+                warBblkAmt);
+
+            Console.WriteLine("{0} Has {1} Health \n",warriorB.Name
+                ,warriorB.Health);
+
+            if (warriorB.Health <= 0)
+            {
+                Console.WriteLine("{0} has DIED and {1} is VICTORIUS\n",
+                    warriorB.Name,
+                    warriorA.Name);
+                return "Game Over";
+            }
+            else return "Fight Again";
+
+        }
+
+    }
+}
---
+++ Fight2Death/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight2Death
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior maximus = new Warrior("Maximus", 1000, 120, 40);
+
+            Warrior spartacus = new Warrior("Spartacus", 1000, 120, 40);
+
+            Battle.StartFight(maximus, spartacus);
+
+            Console.ReadLine();
+        }
+    }
+}
